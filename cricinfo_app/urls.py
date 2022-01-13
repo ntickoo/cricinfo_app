dabdd8904e69ed06b7a1c31e4f969c155d3b052a
Neeraj Tickoo <ntickoo@gmail.com>
2022-01-12 22:35:27 -0500

    Add model, url view mapping

--- cricinfo_app/urls.py
+++ cricinfo_app/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/'           ,   admin.site.urls         ),
-    path('api/v0/games/'    ,   include("game.urls")    )
+    path('api/v1/game-results'    ,   include("game.urls")    )
 ]
